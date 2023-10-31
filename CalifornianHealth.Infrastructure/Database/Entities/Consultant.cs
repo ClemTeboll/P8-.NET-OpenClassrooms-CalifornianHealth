109dcf0fea0a907a06f3e94bfc48caa4ea20eceb
Clement <clement.llobet@tutanota.com>
2023-10-31 20:14:24 +0100

    create-consultant-dto-and-entity

---
+++ CalifornianHealth.Infrastructure/Database/Entities/Consultant.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CalifornianHealth.Infrastructure.Database.Entities
+{
+    public partial class Consultant
+    {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Speciality { get; set; }
+
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Consultant>(c =>
+            {
+                c.HasKey(x => x.Id);
+                c.Property(x => x.FirstName).HasMaxLength(100);
+                c.Property(x => x.LastName).HasMaxLength(100);
+                c.Property(x => x.Speciality).HasMaxLength(50);
+                c.ToTable(nameof(Consultant));
+            });
+        }
+
+    }
+}
