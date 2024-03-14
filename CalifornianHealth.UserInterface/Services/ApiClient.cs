e9c02b9e4a06240c74be9952f842707a528dee7b
Clement <clement.llobet@tutanota.com>
2024-03-15 00:00:42 +0100

    modify-api-client

--- CalifornianHealth.UserInterface/Services/ApiClient.cs
+++ CalifornianHealth.UserInterface/Services/ApiClient.cs
@@ -5,15 +5,29 @@ namespace CalifornianHealth.UserInterface.Services;
 public class ApiClient
 {
     private readonly HttpClient _httpClient;
+    private readonly string _baseUrl;
 
-    public ApiClient(HttpClient httpClient)
+    public ApiClient(HttpClient httpClient, IWebHostEnvironment env)
     {
         _httpClient = httpClient;
+        _baseUrl = GetBaseUrl(env);
+    }
+
+    private static string GetBaseUrl(IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            return "http://localhost:5013";
+        }
+        else
+        {
+            return "https://localhost:7090";
+        }
     }
 
     public async Task<List<ConsultantOutputDto>> GetAllConsultants()
     {
-        var result = await _httpClient.GetAsync("https://localhost:7090/api/Consultant");
+        var result = await _httpClient.GetAsync($"{_baseUrl}/api/Consultant");
         result.EnsureSuccessStatusCode();
 
         var data = await result.Content.ReadAsStreamAsync();
@@ -27,7 +41,7 @@ public class ApiClient
 
     public async Task<List<ConsultantCalendarOutputDto>> GetAllConsultantCalendars()
     {
-        var result = await _httpClient.GetAsync("https://localhost:7090/api/ConsultantCalendar");
+        var result = await _httpClient.GetAsync($"{_baseUrl}/api/ConsultantCalendar");
         result.EnsureSuccessStatusCode();
 
         var data = await result.Content.ReadAsStreamAsync();
